dda7ac6000c9916d73e21f83ca44e8f8aac9147d
hanugogi <hyinwoojjang@gmail.com>
2022-12-14 22:50:21 +0900

    Feature: add output by base string
    추후 cls()를 통해 Pretty 한 출력을 구현하기 위한 기반이 되는 부분 구현

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+
+from copy import deepcopy
 from colors import color
 
 parser = argparse.ArgumentParser(prog='ProgramName',
@@ -28,9 +30,9 @@ parser.add_argument('-t',
 parser.add_argument('-D',
                     '--print-divider',
                     type=int,
-                    default=12,
+                    default=1,
                     choices=[1, 2, 3, 4, 6, 12],
-                    help='출력 시 1년을 입력한 개월 단위로 쪼개서 보여줌(기본값: 12)')
+                    help='출력 시 1년을 입력한 개월 단위로 쪼개서 보여줌(기본값: 1)')
 
 args = parser.parse_args()
 print(color("이자", style='bold') + ':', color("{} {}%", fg='green').format("연" if args.interest_type == "yearly" else "월",
@@ -46,11 +48,33 @@ total_months = 0
 save_amount = months * args.monthly_saving
 current_amount = 0
 
-row = 12 // args.print_divider
+OUTPUT_BASE_0_ROWS = 12 // args.print_divider
+OUTPUT_BASE_0_PADDING = 12
 
+OUTPUT_BASE_0_DESC = "소요 기간 ({}年):  "
+OUTPUT_BASE_0_FORMAT = "{}月"
 
 output_bases = []
-output_bases[0] = color("소요시간", style='bold') + ':'
+
+output_base = deepcopy(OUTPUT_BASE_0_DESC)
+for month in range(OUTPUT_BASE_0_ROWS):
+    output_base += OUTPUT_BASE_0_FORMAT
+    output_base += ' ' * (OUTPUT_BASE_0_PADDING -
+                          max(0, len(OUTPUT_BASE_0_FORMAT)))
+
+output_bases.append(output_base)
+print(output_bases[0])
+
+for test_epoch in range(3):
+    numeric_output = map(lambda month: month * args.print_divider,
+                         range(OUTPUT_BASE_0_ROWS))
+
+    output = deepcopy(output_bases[0]).format(total_months // 12, *numeric_output)
+    print(output)
+
+    total_months += 12
+
+exit(0)
 # TODO - 모인 금액 제대로 계산 안됨
 #     - 중간에 입력을 통해 current_amount에 변화 줄 수 있도록 (추가 수입 or 지출)
 #     - 임금 상승률, n년 단위로 금리 변동 기능 추가
@@ -59,15 +83,14 @@ while current_amount < args.target_amount:
     current_amount += save_amount
     current_amount += current_amount * args.interest
 
-    padding = 16
-
     output = ""
 
     word = color("소요시간", style='bold') + ':'
     word += ' ' * (padding - max(0, len(word)))
 
     output += word
-    output += color('{}달 ({}년)'.format(total_months, total_months/12.0), fg='magenta')
+    output += color('{}달 ({}년)'.format(total_months,
+                    total_months/12.0), fg='magenta')
     output += '\n'
 
     output += color("모인 금액", style='bold') + ':'
