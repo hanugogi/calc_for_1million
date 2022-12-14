5d6ee62a9701d74561949e7ac1aca64b8b1326c1
hanugogi <hyinwoojjang@gmail.com>
2022-12-14 21:51:11 +0900

    Pretty Output(Not finished)

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from colors import color
 
@@ -7,7 +8,7 @@ parser = argparse.ArgumentParser(prog='ProgramName',
 parser.add_argument('-m',
                     '--monthly-saving',
                     type=int,
-                    default=200,
+                    default=2000000,
                     help='월 저축 금액')
 parser.add_argument('-i',
                     '--interest',
@@ -24,12 +25,20 @@ parser.add_argument('-t',
                     type=int,
                     default=100000000,
                     help='목표 금액 (기본값: 1억)')
+parser.add_argument('-D',
+                    '--print-divider',
+                    type=int,
+                    default=12,
+                    choices=[1, 2, 3, 4, 6, 12],
+                    help='출력 시 1년을 입력한 개월 단위로 쪼개서 보여줌(기본값: 12)')
 
 args = parser.parse_args()
 print(color("이자", style='bold') + ':', color("{} {}%", fg='green').format("연" if args.interest_type == "yearly" else "월",
-                                                               args.interest * 100))
-print(color("월 저축액", style='bold') + ':', color(format(args.monthly_saving, ','), fg='cyan'))
-print(color("목표 금액", style='bold') + ':', color(format(args.target_amount, ','), fg='blue', style='bold'))
+                                                                          args.interest * 100))
+print(color("월 저축액", style='bold') + ':',
+      color(format(args.monthly_saving, ','), fg='cyan'))
+print(color("목표 금액", style='bold') + ':',
+      color(format(args.target_amount, ','), fg='blue', style='bold'))
 
 months = 12 if 'yearly' == args.interest_type else 1
 total_months = 0
@@ -37,16 +46,34 @@ total_months = 0
 save_amount = months * args.monthly_saving
 current_amount = 0
 
-#TODO - 모인 금액 제대로 계산 안됨
+row = 12 // args.print_divider
+
+
+output_bases = []
+output_bases[0] = color("소요시간", style='bold') + ':'
+# TODO - 모인 금액 제대로 계산 안됨
 #     - 중간에 입력을 통해 current_amount에 변화 줄 수 있도록 (추가 수입 or 지출)
 #     - 임금 상승률, n년 단위로 금리 변동 기능 추가
 while current_amount < args.target_amount:
-        total_months += months
-        current_amount += save_amount
-        current_amount *= args.interest
+    total_months += months
+    current_amount += save_amount
+    current_amount += current_amount * args.interest
 
-        print()
-        print(color("소요시간", style='bold') + ':', color('{}달 ({}년)'.format(total_months, total_months/12.0), fg='magenta'))
-        print(color("모인 금액", style='bold') + ':', color(format(current_amount, ','), fg='red'))
-        
-        input()
+    padding = 16
+
+    output = ""
+
+    word = color("소요시간", style='bold') + ':'
+    word += ' ' * (padding - max(0, len(word)))
+
+    output += word
+    output += color('{}달 ({}년)'.format(total_months, total_months/12.0), fg='magenta')
+    output += '\n'
+
+    output += color("모인 금액", style='bold') + ':'
+    output += color(format(int(current_amount), ','), fg='red')
+    output += '\n'
+
+    input()
+    os.system('cls')
+    print(output)
